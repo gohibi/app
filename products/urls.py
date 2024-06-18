e899e35c8ea0dc62cf61368bbe62494b456ad735
gohibi <andrejoelgohi@gmail.com>
2024-06-18 20:11:05 +0000

    products tags main

--- products/urls.py
+++ products/urls.py
@@ -7,5 +7,5 @@ app_name="products"
 urlpatterns = [
     path('<slug:category_slug>/',views.catalog,name="catalog"),
     path('produit/details/<slug:slug>/<str:pid>',views.product_detail, name="product-detail"),
-    # path('categorie/produit/<slug:slug>',views.product_category,name="product-category")
+   
 ]
