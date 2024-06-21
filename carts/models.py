39d7ec39aa35615c42ea01fe626fdb04640c2c67
gohibi <andrejoelgohi@gmail.com>
2024-06-21 17:25:19 +0000

    ajouter produit au panier

--- carts/models.py
+++ carts/models.py
@@ -1,3 +1,32 @@
 from django.db import models
-
+from users.models import User
+from products.models import Product , Category
 # Create your models here.
+
+class CartQueryset(models.QuerySet):
+    def total_price(self):
+        return sum(cart.product_sell() for cart in self)
+    
+    def total_item(self):
+        if self:
+            return sum(cart.quantity for cart in self)
+        return 0
+    
+
+class Cart(models.Model):
+    user = models.ForeignKey(to=User,on_delete=models.CASCADE,blank=True, null=True)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField(default=0)
+    at_created = models.DateTimeField(auto_now_add=True)
+    key_session = models.CharField(max_length=32,blank=True, null=True)
+    
+    class Meta:
+        verbose_name_plural = "Carts"
+        
+    objects = CartQueryset().as_manager() 
+        
+    def product_sell(self):
+        return round(self.product.price * self.quantity, 0)
+    
+    def __str__(self):
+        return f"Cart {self.user.username} | product {self.product.name} | quantity {self.quantity}"
--- carts/views.py
+++ carts/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from products.models import Product
+from users.models import User
+from carts.models import Cart
 
 # Create your views here.
 
 def add_cart(request,pid):
-    context={}
-    return render(request,"carts/add_cart.html",context)
+    product = Product.objects.get(pid=pid)
+    
+    if request.user.is_authenticated:
+        carts = Cart.objects.filter(user=request.user , product=product)
+        if carts.exists():
+            cart = carts.first()
+            if cart:
+                cart.quantity +=1
+                cart.save()
+        else:
+            Cart.objects.create(user=request.user , product = product , quantity = 1)
+            
+    return redirect(request.META['HTTP_REFERER'])
 
 
 def change_cart(request,pid):
